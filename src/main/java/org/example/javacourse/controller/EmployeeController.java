6ec6a2ad3e5608f93836b7d64a7dbbd847f42964
Ali Nuruyev <133901389+EliNuruyev@users.noreply.github.com>
2024-12-20 12:30:50 +0400

    Update EmployeeController.java

--- src/main/java/org/example/javacourse/controller/EmployeeController.java
+++ src/main/java/org/example/javacourse/controller/EmployeeController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/employee")
+@RequestMapping("/employee3")
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class EmployeeController {
